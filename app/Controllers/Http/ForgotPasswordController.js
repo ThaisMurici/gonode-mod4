2d21f935e0560f64d4456dd151ef73606a9b9fe9
Thais Moraes <murici.thais@gmail.com>
2019-04-05 12:10:08 -0300

    Reseting password on the database

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -33,6 +34,37 @@ class ForgotPasswordController {
       return response.status(error.status).send({
         error: {
           message: 'Something went wrong. Check if informed e-mail is correct.'
+        }
+      })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: {
+            message: 'The recovery token has expired.'
+          }
+        })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (error) {
+      return response.status(error.status).send({
+        error: {
+          message: 'Something went wrong while reseting your password.'
         }
       })
     }

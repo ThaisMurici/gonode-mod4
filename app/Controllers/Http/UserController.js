b80ae07f4aea794599248e3750b38d6cb6b709dc
Thais Moraes <murici.thais@gmail.com>
2019-04-09 10:46:31 -0300

    Adding user addresses

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -5,8 +5,12 @@ const User = use('App/Models/User')
 class UserController {
   async store ({ request }) {
     const userData = request.only(['username', 'email', 'password'])
+    const addresses = request.input('addresses')
 
     const user = await User.create(userData)
+
+    await user.addresses().createMany(addresses)
+
     return user
   }
 }
